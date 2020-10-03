52612230c30ca62f3ec5ca05021df3d02e198f9e
Basheer Kharoti <bsr.anwar@gmail.com>
2020-10-03 10:41:24 +0500

    composite pattern test

---
+++ tests/CompositePatternTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use \App\Factory\Structural\CompositePattern\Form\{
+  Form,
+  InputField,
+  Fieldset
+};
+
+class CompositePatternTest extends TestCase
+{
+  public function testCanCreateADynamicForm()
+  {
+    $form = new Form('users', 'Signup Form', 'http://localhost');
+    $form->add(new InputField('first_name', 'First Name', 'text'));
+    $form->add(new InputField('last_name', 'Last Name', 'text'));
+
+    $fieldSet = new Fieldset('login-details', 'Credentials');
+    $fieldSet->add(new InputField('email', 'Email', 'email'));
+    $fieldSet->add(new InputField('Password', 'password', 'password'));
+
+    $form->add($fieldSet);
+    $this->assertStringContainsStringIgnoringCase('<input type="text" name="first_name" value="" />', $form->render());
+  }
+}
